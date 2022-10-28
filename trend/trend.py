ef525238211eb3d3b52f5eeacb71d27f90a2adc9
umairgillani93 <umairgillani93@gmail.com>
2022-10-29 03:24:48 +0500

    time-series trend

--- trend/trend.py
+++ trend/trend.py
@@ -4,12 +4,15 @@ import numpy as np
 import pandas as pd 
 from pathlib import Path
 import statsmodels
+from statsmodels.tsa.deterministic import DeterministicProcess
+from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 
 from warnings import simplefilter
 simplefilter('ignore')
 
 print('\nImports successful')
+print(f'\nstatsmodels version: {statsmodels.__version__}')
 
 # Set matplotlib defaults
 plt.style.use('seaborn-whitegrid')
@@ -53,9 +56,9 @@ ax = tunnel.plot(style='.', color='0.5')
 moving_avg.plot(
     ax=ax, linewidth=3, title='Tunnel Traffic - 365-Day Moving Average', legend=False)
 
-plt.show()
+#plt.show()
 
-dp = statsmodels.tsa.deterministic.DeterministicProcess(
+dp = DeterministicProcess(
     index = tunnel.index, # dates from training data
     constant = True, # dummy feature for bias
     order = 1, # the time dummy trend
@@ -64,5 +67,35 @@ dp = statsmodels.tsa.deterministic.DeterministicProcess(
 
 X = dp.in_sample() # creates features for the dates given in the 'index' argument
 
+print('X head: {}'.format(X.head()))
 
-X.head()
+y = tunnel['NumVehicles'] # target to predict
+
+model = LinearRegression(fit_intercept=False) # LinearRegression behaves badly with Duplicate features
+# so we need to exclude it here
+
+# fit the linear model
+model.fit(X, y)
+y_pred = pd.Series(model.predict(X), index=X.index)
+print(f'\nPredictinos: {y_pred}')
+
+ax = tunnel.plot(style='.', color='0.5',
+    title='Tunnel Traffic - Linear Trend')
+_ = y_pred.plot(
+    ax = ax,
+    linewidth=3,
+    label='Trend')
+
+
+# lets make a 30-days forecast
+X = dp.out_of_sample(steps=30)
+y_fore = pd.Series(
+    model.predict(X), index=X.index)
+
+print(f'y_fore_head: {y_fore.head()}')
+
+ax = tunnel['2005-05':].plot(title='Tunnel Traffic - Linear Trend Forecast')
+ax = y_pred["2005-05":].plot(ax=ax, linewidth=3, label="Trend")
+ax = y_fore.plot(ax=ax, linewidth=3, label="Trend Forecast", color="C3")
+_ = ax.legend()
+plt.show()
