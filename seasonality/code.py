5a0d5ebfcd086f239fd9236c20c5381989579a39
umairgillani93 <umairgillani93@gmail.com>
2022-10-30 05:28:37 +0500

    time-series seasonality

--- seasonality/code.py
+++ seasonality/code.py
@@ -5,30 +5,20 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from pathlib import Path
 from warnings import filterwarnings
-from sklearn.model_selection import LinearRegression
-from statsmodels.tsa.deterministic import CalenderFourier, DeterministicProcess
+from sklearn.linear_model import LinearRegression
+from statsmodels.tsa.deterministic import CalendarFourier, DeterministicProcess
 filterwarnings('ignore')
 
-print('Imports successful')
-
-def fourier_transform(index, freq, order):
-  # computer fourier transform to the 4thorder (8 new features)
-  # for a series 'y' with daily observations and annual seasonality
-  time = np.arange(len(index), dtype=np.float32)
-  print(f'time: {time}')
-  k = 2 * np.pi * (1 / freq) * time 
-  print(f'k: {k}')
-
-  features = {}
-
-  for i in range(1, order + 1):
-    features.update(
-        {f"sine_{freq}_{i}": np.sine(i * k),
-        f"cos{freq}_{i}": np.cos(i * k),
-          }
-
-  return pd.DataFrame(features, index=index)
-
+def fourier_features(index, freq, order):
+    time = np.arange(len(index), dtype=np.float32)
+    k = 2 * np.pi * (1 / freq) * time
+    features = {}
+    for i in range(1, order + 1):
+        features.update({
+            f"sin_{freq}_{i}": np.sin(i * k),
+            f"cos_{freq}_{i}": np.cos(i * k),
+        })
+    return pd.DataFrame(features, index=index)
 
 # set plotting parameters
 plt.style.use('seaborn-whitegrid')
@@ -54,56 +44,48 @@ plot_params = dict(
 )
 
 def seasonal_plot(X, y, period, freq, ax=None):
-  if ax is None:
-    _, ax = plt.subplots()
-  
-  palette = sns.color_palette('hus1', n_colors=X[period].nunique())
-
-  ax = sns.linplot(
-    x = freq,
-    y = y,
-    hue = period,
-    data = X,
-    ci = False,
-    ax = ax,
-    palette = palette,
-    legend = False
+    if ax is None:
+        _, ax = plt.subplots()
+    palette = sns.color_palette("husl", n_colors=X[period].nunique(),)
+    ax = sns.lineplot(
+        x=freq,
+        y=y,
+        hue=period,
+        data=X,
+        ci=False,
+        ax=ax,
+        palette=palette,
+        legend=False,
     )
-
-  ax.set_title(f'seasonal plot ({period} / {freq})')
-
-  for line, name in zip(ax.line, X[period].unuqie()):
-      y_ = lin.get_ydata()[-1]
-      ax.annotate(
-          name,
-          xy=(1, y_),
-          xytext = (6,0)
-          color = line.get_color(),
-          xycoords = ax.get_yaxis_transform(),
-          textcoords = 'offset points',
-          size = 14,
-          va = 'center'
-          )
-
-  return ax
+    ax.set_title(f"Seasonal Plot ({period}/{freq})")
+    for line, name in zip(ax.lines, X[period].unique()):
+        y_ = line.get_ydata()[-1]
+        ax.annotate(
+            name,
+            xy=(1, y_),
+            xytext=(6, 0),
+            color=line.get_color(),
+            xycoords=ax.get_yaxis_transform(),
+            textcoords="offset points",
+            size=14,
+            va="center",
+        )
+    return ax
 
 
 def plot_periodogram(ts, detrend='linear', ax=None):
-  from scipy.signal import periodogram
-  fs = pd.Timedelta('1Y') / pd.Timedelta('1D')
-  frequencies, spectrum = periodogram(
-      ts,
-      fs=fs,
-      detrend = dtrend,
-      window='boxcar',
-      scaling='spectrum'
-      _
-  
-  if ax is None:
-    _, ax = plt.plot()
-
-
-  ax.step(freqencies, spectrum, color="purple")
+    from scipy.signal import periodogram
+    fs = pd.Timedelta("1Y") / pd.Timedelta("1D")
+    freqencies, spectrum = periodogram(
+        ts,
+        fs=fs,
+        detrend=detrend,
+        window="boxcar",
+        scaling='spectrum',
+    )
+    if ax is None:
+        _, ax = plt.subplots()
+    ax.step(freqencies, spectrum, color="purple")
     ax.set_xscale("log")
     ax.set_xticks([1, 2, 4, 6, 12, 26, 52, 104])
     ax.set_xticklabels(
@@ -123,3 +105,30 @@ def plot_periodogram(ts, detrend='linear', ax=None):
     ax.set_ylabel("Variance")
     ax.set_title("Periodogram")
     return ax
+
+
+
+
+path =  os.getenv('HOME') + '/datasets/book_sales_kaggle/tunnel.csv'
+tunnel = pd.read_csv(path, parse_dates = ['Day'])
+
+print(f'dataframe head: {tunnel.head()}')
+
+tunnel.set_index('Day').to_period('D')
+
+# Lets look at the seasonal plots over a week and over a year
+X = tunnel.copy()
+
+# days within a week
+#X["day"] = X.index.dayofweek  # the x-axis (freq)
+#X["week"] = X.index.week  # the seasonal period (period)
+
+# days within a year
+#X["dayofyear"] = X.index.dayofyear
+#X["year"] = X.index.year
+fig, (ax0, ax1) = plt.subplots(2, 1, figsize=(11, 6))
+seasonal_plot(X, y="NumVehicles", period="week", freq="day", ax=ax0)
+seasonal_plot(X, y="NumVehicles", period="year", freq="dayofyear", ax=ax1);
+
+# show the plot
+plt.show()
